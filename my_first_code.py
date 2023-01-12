4a174ce67117daee0eeffc15f1ec19c57299ca39
Abdoul Balde <116114165+AGBal2@users.noreply.github.com>
2023-01-12 21:51:45 +0100

    Added my first code (.ipynb and .py)

---
+++ my_first_code.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+#this is my first line of code
+print('I go by Abdoul')
+print('Date is Jan. 12.2023')
+
+
+# In[11]:
+
+
+#Write a program to analyse real estate data
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+total_sale = a+b+c+d+e
+
+print(total_sale)
+
+
+# In[12]:
+
+
+average_sale = total_sale / 5
+
+print(average_sale)
+
+
+# In[13]:
+
+
+#determine the missing value x
+
+x = 67000-6500
+print(x)
+
+
+# In[ ]:
+
+
+
+
